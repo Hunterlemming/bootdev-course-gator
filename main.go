eaec77572f9f9ffa69b845825dd04d87b988a403
Hunterlemming <kklemming@gmail.com>
2025-04-18 11:39:59 +0200

    ch1-l3

--- main.go
+++ main.go
@@ -2,22 +2,38 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/hunterlemming/bootdev-course-gator/internal/config"
 )
+
+type state struct {
+	cfg *config.Config
+}
 
 func main() {
 	cfg, err := config.Read()
 	if err != nil {
 		fmt.Println(err.Error())
 	}
+	s := state{cfg: &cfg}
 
-	cfg.SetUser("Krisz")
+	comms := commands{handlers: make(map[string]func(*state, command) error)}
+	comms.register("login", handlerLogin)
 
-	cfg, err = config.Read()
-	if err != nil {
-		fmt.Println(err.Error())
+	if len(os.Args) < 2 {
+		fmt.Println("not enough arguments provided")
+		os.Exit(1)
 	}
 
-	fmt.Println(cfg)
+	cmd := command{
+		name: os.Args[1],
+		args: os.Args[2:],
+	}
+
+	err = comms.run(&s, cmd)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
